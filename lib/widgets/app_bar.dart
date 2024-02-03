04c8b760b14ddca49b7433f36c985a6f644eacd7
Gaurav Kushwaha <gaurav.family04@gmail.com>
2024-02-04 02:38:08 +0530

    Login Page UI Created

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -17,12 +17,15 @@ class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 20),
+      margin: const EdgeInsets.only(top: 24),
       child: AppBar(
         leading: IconButton(
-            onPressed: widget.leadingFunc, icon: Icon(widget.leadingIcon)),
+          onPressed: widget.leadingFunc,
+          icon: Icon(widget.leadingIcon),
+          splashColor: Colors.transparent,
+    highlightColor: Colors.transparent,
+        ),
       ),
-      
     );
   }
 }
