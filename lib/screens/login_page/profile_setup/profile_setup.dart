20e7b9fbab4705cf06e293958e3c5350d8e3f4a6
Gaurav-Kushwaha-1225 <gaurav.family04@gmail.com>
2024-06-11 23:57:41 +0530

    ProfileSetup Page DOB TextField Added

--- lib/screens/login_page/profile_setup/profile_setup.dart
+++ lib/screens/login_page/profile_setup/profile_setup.dart
@@ -26,6 +26,12 @@ class _ProfileSetupState extends State<ProfileSetup> {
   final FocusNode nicknameFocusNode = FocusNode();
   final GlobalKey<FormState> nicknameKey = GlobalKey();
   String? errorNicknameValue;
+
+  final TextEditingController dobTextEditingController =
+      TextEditingController();
+  final FocusNode dobFocusNode = FocusNode();
+  final GlobalKey<FormState> dobKey = GlobalKey();
+  String? errordobValue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -177,6 +183,103 @@ class _ProfileSetupState extends State<ProfileSetup> {
                                 const BorderRadius.all(Radius.circular(12)),
                             gapPadding: 24),
                         hintText: 'Nickname',
+                        hintStyle: GoogleFonts.urbanist(
+                            decoration: TextDecoration.none,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).brightness == Brightness.light
+                                ? Colors.grey.shade600
+                                : Colors.grey.shade300,
+                            letterSpacing: 1.2,
+                            fontStyle: FontStyle.normal),
+                        // TODO: Change fill color according to UI when in focus and dark theme or light theme
+                        fillColor: Theme.of(context).brightness == Brightness.light
+                            ? Constants.lightCardFillColor
+                            : Constants.darkCardFillColor,
+                        filled: true,
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Theme.of(context).brightness == Brightness.light
+                                    ? Constants.lightSecondary
+                                    : Constants.darkSecondary),
+                            borderRadius: const BorderRadius.all(Radius.circular(12)),
+                            gapPadding: 24),
+                        focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).brightness == Brightness.light ? Colors.red.shade600 : Colors.red.shade300), borderRadius: const BorderRadius.all(Radius.circular(12)), gapPadding: 24),
+                        errorBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).brightness == Brightness.light ? Colors.red.shade200 : Colors.red.shade300), borderRadius: const BorderRadius.all(Radius.circular(12)), gapPadding: 24)),
+                  ),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: const EdgeInsets.only(left: 24, right: 24, bottom: 13),
+                child: Form(
+                  key: dobKey,
+                  child: TextFormField(
+                    autofocus: false,
+                    focusNode: dobFocusNode,
+                    controller: dobTextEditingController,
+                    cursorColor:
+                        Theme.of(context).brightness == Brightness.light
+                            ? Constants.lightTextColor
+                            : Constants.darkTextColor,
+                    validator: (value) {
+                      return;
+                    },
+                    onTap: () async {
+                      DateTime date = DateTime(1900);
+                      FocusScope.of(context).requestFocus(new FocusNode());
+
+                      date = (await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1900, 1, 1),
+                          lastDate: DateTime.now()))!;
+
+                      dobTextEditingController.text = date.toString().substring(0, 10);
+                    },
+                    style: GoogleFonts.urbanist(
+                        decoration: TextDecoration.none,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                        color: Theme.of(context).brightness == Brightness.light
+                            ? Constants.lightTextColor
+                            : Constants.darkTextColor,
+                        letterSpacing: 1.2,
+                        fontStyle: FontStyle.normal),
+                    decoration: InputDecoration(
+                        errorText: errordobValue,
+                        errorStyle: GoogleFonts.urbanist(
+                            decoration: TextDecoration.none,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).brightness == Brightness.light
+                                ? Colors.red.shade600
+                                : Colors.red.shade300,
+                            letterSpacing: 1.2,
+                            fontStyle: FontStyle.normal),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 15, horizontal: 15),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Theme.of(context).brightness == Brightness.light
+                                    ? Constants.lightBorderColor
+                                    : Constants.darkBorderColor),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(12)),
+                            gapPadding: 24),
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                          },
+                          child: Icon(
+                            Icons.date_range_rounded,
+                            color: Theme.of(context).brightness == Brightness.light
+                                ? Colors.grey.shade600
+                                : Colors.grey.shade300,
+                            size: 18,
+                          ),
+                        ),
+                        hintText: 'Date of Birth',
                         hintStyle: GoogleFonts.urbanist(
                             decoration: TextDecoration.none,
                             fontSize: 15,
