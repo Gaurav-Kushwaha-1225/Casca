38b53a9d3c30c8a154895f6fd941e70dcdadf942
Gaurav-Kushwaha-1225 <gaurav.family04@gmail.com>
2024-07-06 20:49:57 +0530

    main.dart updated, renamed login_with_widget.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:Casca/features/authentication/data/data_sources/user_database.dart';
+import 'package:Casca/features/authentication/data/repository/user_repository_impl.dart';
+import 'package:Casca/features/authentication/domain/usecases/login_user.dart';
+import 'package:Casca/features/authentication/domain/usecases/signup_user.dart';
 import 'package:Casca/utils/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'config/routes/routes.dart';
 
@@ -16,15 +20,21 @@ class Casca extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp
-      ]);
-    return MaterialApp.router(
-      title: 'Casca',
-      themeMode: ThemeMode.system,
-      theme: CascaTheme.lightTheme,
-      darkTheme: CascaTheme.darkTheme,
-      routerConfig: CascaRouter().router,
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<LoginUser>(
+            create: (_) => LoginUser(UserRepositoryImpl(CascaUsersDB()))),
+        RepositoryProvider<SignupUser>(
+            create: (_) => SignupUser(UserRepositoryImpl(CascaUsersDB()))),
+      ],
+      child: MaterialApp.router(
+        title: 'Casca',
+        themeMode: ThemeMode.system,
+        theme: CascaTheme.lightTheme,
+        darkTheme: CascaTheme.darkTheme,
+        routerConfig: CascaRouter().router,
+      ),
     );
   }
 }
