874be11d6ba52cfc27c7b2a172a8bbaa4225781d
Gaurav Kushwaha <gaurav.family04@gmail.com>
2026-01-23 00:14:46 +0530

    updates

---
+++ lib/config/routes/environment.dart
@@ -0,0 +1,5 @@
+class Environment {
+  static const String appwriteProjectId = '68e6a416001d08cd24bb';
+  static const String appwriteProjectName = 'Casca';
+  static const String appwritePublicEndpoint = 'https://fra.cloud.appwrite.io/v1';
+}
